f79ed5874ba3db4220bb4a7f3f7dff71a0284369
Tomáš Juřena <jurenatomas@gmail.com>
2025-10-09 08:34:43 +0200

    Remove extra repository files
    - Removes hard copy of RoboCupMS (to be replaced by submodule)
    - Removes __MACOSX as not needed
    - Updates the .gitignore to exclude __MACOSX in future
    
    Signed-off-by: Tomáš Juřena <jurenatomas@gmail.com>

--- RoboCupMS/Setup/db_setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE robocup;
-
-CREATE user 'robocup_root'@'%' identified BY 'a63W9bXZYhcwAT9B';
-
-GRANT all ON robocup.* TO 'robocup_root'@'%'; 
--- RoboCupMS/Setup/ssl_certificate_setup.sh
+++
@@ -1,8 +0,0 @@
-# 1) zpusob - self signed certificate 
-# vygenerovani ssl certifikatu a vlozeni do keystoru
-keytool -genkeypair -alias springboot -keyalg RSA -keysize 4096 -storetype PKCS12 -keystore robocupms.p12 -validity 3650 -storepass f4R03eRRG3
-
-# 2) zpusob - vygenerovani certifikatu z privatniho klice a csr
-openssl x509 -req -days 365 -in robocupms.csr -signkey robocupms.key -out robocupms.crt
-# heslo: f4R03eRRG3
-openssl pkcs12 -export -in robocupms.crt -inkey robocupms.key -out robocupms.p12 -name springboot
